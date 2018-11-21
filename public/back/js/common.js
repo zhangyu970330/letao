ddc148f9435f5e218515108a843ea86ca5cc1d45
zhangyu970330 <1220610204@qq.com>
2018-11-21 18:53:42 +0800

    静态结构公共样式完成

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -22,4 +22,19 @@ $(function () {
 
 
     });
+
+
+    // 公用的功能
+    // 1.左侧二级切换功能
+    $('#category').click(function () {
+        $(this).next().stop().slideToggle();
+    })
+
+    // 2. 左侧菜单切换功能
+    $('.lt_topbar .icon_left').click(function () {
+        // 让左侧侧边菜单切换  toggleClass 切换类
+        $('.lt_aside').toggleClass("hidemenu");
+        $('.lt_topbar').toggleClass("hidemenu");
+        $('.lt_main').toggleClass("hidemenu");
+    })
 })
